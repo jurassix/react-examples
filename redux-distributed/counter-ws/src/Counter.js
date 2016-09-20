5a33612b5f2752c735030565ece4c101f0123d7b
Clinton Ayres <clinton.ayres@theice.com>
2016-09-19 22:05:10 -0400

    initial build out

--- redux-distributed/counter-ws/src/Counter.js
+++ redux-distributed/counter-ws/src/Counter.js
@@ -1,37 +1,107 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {render} from 'react-dom';
 import {Provider, connect} from 'react-redux';
 import {createStore} from 'redux';
+import {createSelector} from 'reselect';
+import thunk from 'redux-thunk';
+import compose from 'lodash/flowRight';
+import {createPeer, connectToPeer, send} from './createPeer';
 
-const Counter = ({value, dispatch}) => {
-  <div>
-    <button onClick={() => dispatch({type: 'DECREMENT'})}>-</button>
-    <span>{value}</span>
-    <button onClick={() => dispatch({type: 'INCREMENT'})}>+</button>
-  </div>
-};
-
-const reducer = (state = {value: 0}, action) => {
-  if (action.type === 'INCREMENT') {
-    return {
-      ...state,
-      value: state.value + 1,
-    };
-  } else if (action.type === 'DECREMENT') {
-    return {
-      ...state,
-      value: state.value - 1,
-    };
+class Peer extends React.Component {
+  static propTypes = {
+    connectToPeer: PropTypes.func,isRequired,
   }
-  return state;
+
+  render() {
+    return (
+      <div>
+        <label>
+          PeerID to connect to:
+        </label>
+        <input type="text" ref={(r) => this._input = r} />
+        <button onClick={this.handleConnect}>Connect</button>
+      </div>
+    );
+  }
+
+  handleConnect = () => {
+    this.props.connectToPeer(this._input.value);
+    this._input.value = '';
+  }
 }
 
-const store = createStore(reducer, {value: 0});
+const Counter = ({value, onIncrement, onDecrement}) => (
+  <div>
+    <button onClick={onDecrement}>-</button>
+    <span>{value}</span>
+    <button onClick={onIncrement}>+</button>
+  </div>
+);
 
-const connectedCounter = connect()(<Counter />);
+const reduceValue = (value = 0, action) => {
+  if (action.type === 'INCREMENT') {
+    return value + 1;
+  }
+  if (action.type === 'DECREMENT') {
+    return value - 1;
+  }
+  return value;
+}
+
+const reducePeer = (peer, action) => {
+  if (action.type === '@@PEER_INIT') {
+    return createPeer(action.peerOptions);
+  }
+  return peer;
+}
+
+const reducers = (state, action) => ({
+  value: reduceValue(state.value, action),
+});
+
+const actions = {
+  onIncrement: () => ({type: 'INCREMENT'}),
+  onDecrement: () => ({type: 'DECREMENT'}),
+  initPeer: (peerOptions) => ({type: '@@PEER_INIT', peerOptions}),
+  connectToPeer: (remotePeerId) => (dispatch, getState) => {
+    const {peer} = getState();
+    connectToPeer(
+      peer,
+      remotePeerId,
+      (data) => dispatch({type: 'PEER_DATA_RECEIVE', data})
+    );
+  },
+  sendMessage: (message) => (dispatch, getState) => {
+    const {peer} = getState();
+    send(peer)(message);
+  }
+};
+
+const selectValue = (state) => state.value;
+
+const mapStateToProps = (state) => ({
+  value: selectValue,
+});
+
+const store = createStore(
+  reducers,
+  {
+    value: 0,
+  },
+  compose(
+    applyMiddleware(thunk),
+    typeof window === 'object' &&
+    typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f,
+  )
+);
+
+const PeerContainer = connect(null, actions)(<Peer />);
+const CounterContainer = connect(mapStateToProps, actions)(<Counter />);
+
 render(
   <Provider store={store}>
-    <Counter />
+    <PeerContainer />
+    <CounterContainer />
   </Provider>
   , document.getElementById('root')
 );
--- redux-distributed/counter-ws/src/index.js
+++ redux-distributed/counter-ws/src/index.js
@@ -1,53 +1,107 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
 import {render} from 'react-dom';
 import {Provider, connect} from 'react-redux';
 import {createStore} from 'redux';
-import './index.css';
-import {
-  connectToPeer,
-  createPeer,
-  send,
-} from './createPeer';
+import {createSelector} from 'reselect';
+import thunk from 'redux-thunk';
+import compose from 'lodash/flowRight';
+import {createPeer, connectToPeer, send} from './createPeer';
 
-const Counter = ({value, dispatch}) => (
+class Peer extends React.Component {
+  static propTypes = {
+    connectToPeer: PropTypes.func,isRequired,
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          PeerID to connect to:
+        </label>
+        <input type="text" ref={(r) => this._input = r} />
+        <button onClick={this.handleConnect}>Connect</button>
+      </div>
+    );
+  }
+
+  handleConnect = () => {
+    this.props.connectToPeer(this._input.value);
+    this._input.value = '';
+  }
+}
+
+const Counter = ({value, onIncrement, onDecrement}) => (
   <div>
-    <button onClick={() => dispatch({type: 'DECREMENT'})}>-</button>
+    <button onClick={onDecrement}>-</button>
     <span>{value}</span>
-    <button onClick={() => dispatch({type: 'INCREMENT'})}>+</button>
+    <button onClick={onIncrement}>+</button>
   </div>
 );
 
-const reducer = (state = {value: 0}, action) => {
+const reduceValue = (value = 0, action) => {
   if (action.type === 'INCREMENT') {
-    return {
-      ...state,
-      value: state.value + 1,
-    };
-  } else if (action.type === 'DECREMENT') {
-    return {
-      ...state,
-      value: state.value - 1,
-    };
+    return value + 1;
   }
-  return state;
+  if (action.type === 'DECREMENT') {
+    return value - 1;
+  }
+  return value;
 }
 
-const store = createStore(reducer, {value: 0});
+const reducePeer = (peer, action) => {
+  if (action.type === '@@PEER_INIT') {
+    return createPeer(action.peerOptions);
+  }
+  return peer;
+}
 
-const mapStateToProps = (state) => state;
-const ConnectedCounter = connect(mapStateToProps)(Counter);
+const reducers = (state, action) => ({
+  value: reduceValue(state.value, action),
+});
+
+const actions = {
+  onIncrement: () => ({type: 'INCREMENT'}),
+  onDecrement: () => ({type: 'DECREMENT'}),
+  initPeer: (peerOptions) => ({type: '@@PEER_INIT', peerOptions}),
+  connectToPeer: (remotePeerId) => (dispatch, getState) => {
+    const {peer} = getState();
+    connectToPeer(
+      peer,
+      remotePeerId,
+      (data) => dispatch({type: 'PEER_DATA_RECEIVE', data})
+    );
+  },
+  sendMessage: (message) => (dispatch, getState) => {
+    const {peer} = getState();
+    send(peer)(message);
+  }
+};
+
+const selectValue = (state) => state.value;
+
+const mapStateToProps = (state) => ({
+  value: selectValue,
+});
+
+const store = createStore(
+  reducers,
+  {
+    value: 0,
+  },
+  compose(
+    applyMiddleware(thunk),
+    typeof window === 'object' &&
+    typeof window.devToolsExtension !== 'undefined' ? window.devToolsExtension() : f => f,
+  )
+);
+
+const PeerContainer = connect(null, actions)(<Peer />);
+const CounterContainer = connect(mapStateToProps, actions)(<Counter />);
+
 render(
   <Provider store={store}>
-    <ConnectedCounter />
+    <PeerContainer />
+    <CounterContainer />
   </Provider>
   , document.getElementById('root')
 );
-
-const peer = createPeer('one');
-const peerTwo = createPeer('two');
-
-connectToPeer(peer, peerTwo);
-connectToPeer(peerTwo, peer);
-
-setTimeout(() => send(peer)({message: 'p1 hello world'}), 2000);
-setTimeout(() => send(peerTwo)({message: 'p2 hello world'}), 4000);
